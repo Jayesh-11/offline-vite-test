6ff9dbc95cc37d318aac2d404b2c04c5ee59ca0e
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2024-07-06 08:44:58 +0530

    added support for images

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
-    devOptions: {
-      enabled: true
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
     }
   })],
 })
